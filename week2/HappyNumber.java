93fb06836135c4f8bfd2ceb272c0e6dc3a172fa1
lunavictor068 <lunavictor068@gmail.com>
2019-05-07 23:29:40 -0600

    Finished HappyNumber

--- week2/HappyNumber.java
+++ week2/HappyNumber.java
@@ -1,17 +1,39 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class HappyNumber {
 
-    public boolean isHappy(int n) {
-
+    public static void main(String[] args){
+        System.out.println(isHappy(19));
     }
 
-    public int sumOfSquares(int n){
-
+    public static boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)){
+            seen.add(n);
+            n = sumOfSquares(n);
+        }
+        return n == 1;
     }
 
-    public int[] toIntArray(int n){
+    public static int sumOfSquares(int n){
+        ArrayList<Integer> integers = toIntArray(n);
+        int sum = 0;
+        for (Integer integer:integers)
+            sum += Math.pow(integer, 2);
+        return sum;
+    }
 
+    public static ArrayList<Integer> toIntArray(int n){
+        ArrayList<Integer> integers = new ArrayList<>();
+        while (n > 9){
+            integers.add(n % 10);
+            n = n / 10;
+        }
+        integers.add(n);
+        return integers;
     }
 
 }
